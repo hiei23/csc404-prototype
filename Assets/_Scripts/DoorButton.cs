8dc8ebce1bb1e7b0e6af593c98c335614e42108e
dakaringer <dakaringer@gmail.com>
2017-02-25 13:01:39 -0500

    Add door + button

---
+++ Assets/_Scripts/DoorButton.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorButton : MonoBehaviour {
+
+    private Animator animator;
+
+	// Use this for initialization
+	void Start () {
+        animator = gameObject.GetComponentInParent<Animator>();
+    }
+	
+	void OnTriggerEnter(Collider col)
+    {
+        if (col.gameObject.tag == "Player1" || col.gameObject.tag == "Player2")
+        {
+            animator.SetTrigger("Open");
+        }
+    }
+}
