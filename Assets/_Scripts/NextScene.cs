18bb3ced2243bf80aa9e5197ba804592e5e01283
SeanWiz <triclopszx@gmail.com>
2017-02-27 15:01:56 -0500

    Add Title Screen

---
+++ Assets/_Scripts/NextScene.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextScene : MonoBehaviour {
+
+    public int sceneIndex = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(Input.GetAxisRaw("Jump") > 0)
+            SceneManager.LoadScene(sceneIndex);
+    }
+}
