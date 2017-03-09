37385a24b568009514231281f4d451df64f85891
SeanWiz <triclopszx@gmail.com>
2017-03-08 22:10:52 -0500

    Breakable Objects stuff

---
+++ Assets/Breakable.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Breakable : MonoBehaviour {
+
+    public GameObject pieces;
+
+    void OnCollisionEnter(Collision collision){
+        if (collision.gameObject.tag == "Player1" || collision.gameObject.tag == "Player2") {
+            Instantiate(pieces, transform.position, transform.rotation);
+            gameObject.SetActive(false);
+        }
+    }
+
+}
