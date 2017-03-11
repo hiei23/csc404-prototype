bafdbf51083a4525132287d21de6b679d6704acd
SeanWiz <triclopszx@gmail.com>
2017-03-11 15:04:46 -0500

    Only Breaks at Given Force

--- Assets/Breakable.cs
+++ Assets/Breakable.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 public class Breakable : MonoBehaviour {
 
     public GameObject pieces;
+    public float force;
 
     void OnCollisionEnter(Collision collision){
-        if (collision.gameObject.tag == "Player1" || collision.gameObject.tag == "Player2") {
+        if (collision.rigidbody.velocity.magnitude > force && 
+                (collision.gameObject.tag == "Player1" || 
+                 collision.gameObject.tag == "Player2")) {
             Instantiate(pieces, transform.position, transform.rotation);
             gameObject.SetActive(false);
         }
