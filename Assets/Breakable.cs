dbb8bd5a6a245aa13977a888d427ffc024cac3e2
SeanWiz <triclopszx@gmail.com>
2017-03-12 19:38:15 -0400

    Shouldnt crash now I hope

--- Assets/Breakable.cs
+++ Assets/Breakable.cs
@@ -7,12 +7,18 @@ public class Breakable : MonoBehaviour {
     public GameObject pieces;
     public float force;
 
+    Rigidbody rb;
+
     void OnCollisionEnter(Collision collision){
-        if (collision.rigidbody.velocity.magnitude > force && 
-                (collision.gameObject.tag == "Player1" || 
-                 collision.gameObject.tag == "Player2")) {
-            Instantiate(pieces, transform.position, transform.rotation);
-            gameObject.SetActive(false);
+        rb = collision.gameObject.GetComponent<Rigidbody>();
+        if (rb) {
+            if (rb.velocity.magnitude > force &&
+                (collision.gameObject.tag == "Player1" ||
+                 collision.gameObject.tag == "Player2"))
+            {
+                Instantiate(pieces, transform.position, transform.rotation);
+                gameObject.SetActive(false);
+            }
         }
     }
 
