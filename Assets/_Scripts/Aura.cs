7082b5f17ad785edfa4a5be3d82fca8dcb272f7b
hiei23 <yuikazuga_25@hotmail.co.uk>
2017-03-13 19:26:48 -0400

    updated aura script

--- Assets/_Scripts/Aura.cs
+++ Assets/_Scripts/Aura.cs
@@ -5,15 +5,16 @@ using UnityEngine;
 public class Aura : MonoBehaviour
 {
     // Use this for initialization
-    private ParticleSystem ps;
+    public ParticleSystem jumpAura;
+    public ParticleSystem slingAura;
     private Component parent;
+
     void Start()
     {
         Component child;
-        parent = GetComponentInParent<Rigidbody>();
-        child = GetComponentInChildren<ParticleSystem>();
-        ps = child.GetComponent<ParticleSystem>();
-        ps.Stop();
+        parent = GetComponent<Rigidbody>();
+        slingAura.Stop();
+        jumpAura.Stop();
     }
 
     // Update is called once per frame
@@ -22,16 +23,29 @@ public class Aura : MonoBehaviour
 
         if ((Input.GetAxisRaw("JumpP1") == 1) && (parent.CompareTag("Player1")))
         {
-            ps.Play();
+            jumpAura.Play();
             return;
         }
 
         if ((Input.GetAxisRaw("JumpP2") == 1) && (parent.CompareTag("Player2")))
         {
-            ps.Play();
+            jumpAura.Play();
             return;
         }
 
-        ps.Stop();
+        if ((Input.GetAxisRaw("ReturnP1") == 1) && (parent.CompareTag("Player1")))
+        {
+            slingAura.Play();
+            return;
+        }
+
+        if ((Input.GetAxisRaw("ReturnP2") == 1) && (parent.CompareTag("Player2")))
+        {
+            slingAura.Play();
+            return;
+        }
+
+        slingAura.Stop();
+        jumpAura.Stop();
     }
 }
