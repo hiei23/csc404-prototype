dd64355c554ba48b96b06e43470040223aa787f7
escipv27 <social_apple@hotmail.com>
2017-03-13 10:00:15 -0400

    level2_map1
    + counting hidden snack object

---
+++ Assets/Breakable2.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Breakable2 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GameObject[] listBreakableObject = GameObject.FindGameObjectsWithTag("Breakable");
+
+        // select random breakable object and assing 'Snack' tag
+
+    }
+	
+
+}
