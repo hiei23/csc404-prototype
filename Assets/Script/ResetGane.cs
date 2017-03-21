6979299e225f00f9f17f5b0906962b4a71823b5c
hiei23 <yuikazuga_25@hotmail.co.uk>
2017-03-21 17:10:40 -0400

    updated auras, added new scene with fireworks, updated title menu

---
+++ Assets/Script/ResetGane.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ResetGane : MonoBehaviour {
+
+    public int sceneIndex = 0;
+    private string MAIN_MENU_SCENE = "Title";
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxisRaw("Submit") > 0)
+        {
+            SceneManager.LoadScene(MAIN_MENU_SCENE);   
+        }
+
+    }
+}
