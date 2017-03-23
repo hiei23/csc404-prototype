3f45aca6388d2424b50eefa302dbf2bf620a729e
escipv27 <social_apple@hotmail.com>
2017-03-22 22:54:25 -0400

    timer
    timer added

---
+++ Assets/_Scripts/Timer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+
+    public Text timerDisplay;
+    public float remainingTime = 120.0f;
+
+	// Use this for initialization
+	void Start () {
+        timerDisplay.text = remainingTime.ToString("F1");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        remainingTime -= Time.deltaTime;
+        timerDisplay.text = remainingTime.ToString("F1");
+
+        if (remainingTime <= 0.0f)
+        {
+            timeEnd();
+        }
+	}
+
+    void timeEnd()
+    {
+
+    }
+}
