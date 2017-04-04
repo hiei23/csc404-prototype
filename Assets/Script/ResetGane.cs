4556884b1800da7bbb6b55aaae4ed4a49b8bbb79
hiei23 <yuikazuga_25@hotmail.co.uk>
2017-04-04 18:49:39 -0400

    added button press sound to endscene 2

--- Assets/Script/ResetGane.cs
+++ Assets/Script/ResetGane.cs
@@ -6,10 +6,13 @@ using UnityEngine.SceneManagement;
 public class ResetGane : MonoBehaviour {
 
     private string MAIN_MENU_SCENE = "Title";
+    AudioSource start_button_sound;
     // Use this for initialization
     void Start()
     {
-
+        AudioSource[] sources = FindObjectsOfType<AudioSource>();
+        start_button_sound = sources[0];
+        start_button_sound.Stop();
     }
 
     // Update is called once per frame
@@ -17,6 +20,7 @@ public class ResetGane : MonoBehaviour {
     {
         if (Input.GetAxisRaw("Submit") > 0)
         {
+            start_button_sound.Play();
             SceneManager.LoadScene(MAIN_MENU_SCENE);   
         }
 
