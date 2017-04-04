385d0daa173928f2312bb4e9b82b136ea33cd43d
hiei23 <yuikazuga_25@hotmail.co.uk>
2017-04-04 17:58:54 -0400

    added instructions scene

---
+++ Assets/_Scripts/GoToMainLevel.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GoToMainLevel : MonoBehaviour {
+
+    // Use this for initialization
+    private string MAIN_LEVEL = "level2_map1";
+    AudioSource start_button_sound;
+
+    void Start () {
+        AudioSource[] sources = FindObjectsOfType<AudioSource>();
+        start_button_sound = sources[0];
+        start_button_sound.Stop();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetAxisRaw("Submit") > 0)
+        {
+            start_button_sound.Play();
+            SceneManager.LoadScene(MAIN_LEVEL);
+        }
+            
+    }
+}
